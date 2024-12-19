39c8d8bd79f9ac082cea588a087eb08eeee2f118
Hugo <alterisateur@gmail.com>
2024-12-19 12:13:05 +0100

    new comment "antiwoke"

--- game_scripts/comments/commentscript_antiwoke.gd
+++ game_scripts/comments/commentscript_antiwoke.gd
@@ -2,20 +2,26 @@ extends CommentScript
 
 # Retourner true si le commentaire peut être affiché
 func is_valid(day_result: DayResult) -> bool:
-	
-	if day_result.today_broken_rules <= 0:
-		return true
+	var nbOfWoke: int = 0
+	for item in day_result.total_stored_items:
+		var book: Book = item as Book
+		if book == null: continue
 		
-
+		if "Woke" in book.data.tags:
+				nbOfWoke += 1
+		
+		if nbOfWoke >= 3:
+			return true
+	
 	return false
 
 # Le contenu du commentaire
 func get_body() -> String:
-	return "Un excellent travail de notre nouvel employé de la bibliothèque."
+	return "Bibliothèque de wokiste ! Je vais couper les subventions."
 
 # L'auteurice du commentaire
 func get_author() -> String:
-	return "@your_work_husband"
+	return "@lemaire"
 
 # L'avatar du commentaire
 func get_avatar() -> Texture2D:
